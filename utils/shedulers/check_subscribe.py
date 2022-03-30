7aba654b53ca81d8325f09b485b7585bf22e1d02
theGeekBeard <thegeekbeard47@gmail.com>
2022-03-30 20:14:50 +0400

    Update сheck_subscribe

--- utils/shedulers/check_subscribe.py
+++ utils/shedulers/check_subscribe.py
@@ -11,8 +11,8 @@ from utils.mailings import notify_operator_about_delete_user
 async def check_access():
     while True:
 
-        if datetime.now(pytz.timezone('Europe/Moscow')).hour == 13 and datetime.now(
-                pytz.timezone('Europe/Moscow')).minute == 0:
+        if datetime.now(pytz.timezone('Europe/Moscow')).hour == 19 and datetime.now(
+                pytz.timezone('Europe/Moscow')).minute == 20:
             print(1)
             text = _("Привет, {name}! Сообщаю, что пора продлить вашу подписку на "
                      "телеграм-канал 'Пой со мной'. До окончания подписки осталось всего {day}. Сейчас у вас "
@@ -26,43 +26,53 @@ async def check_access():
             }
 
             for items in await db.get_users():
+                print(items)
                 if items[1] == datetime.now().date():
+
                     print(235)
-                    text_del = _("Привет, {name}! Сегодня у вас истек срок подписки на телеграм-канал “Пой со мной”. Мы, "
-                             "я вынуждены удалить вас из канала, за то что вы не продлили подписку, таковы правила. В "
-                             "любой момент вы можете вернуться к нам. Для этого  нажмите “Хочу подписку” в Меню. Буду "
-                             "ждать вас!").format(name=items[2])
+                    try:
+                        text_del = _(
+                            "Привет, {name}! Сегодня у вас истек срок подписки на телеграм-канал “Пой со мной”. Мы, "
+                            "я вынуждены удалить вас из канала, за то что вы не продлили подписку, таковы правила. В "
+                            "любой момент вы можете вернуться к нам. Для этого  нажмите “Хочу подписку” в Меню. Буду "
+                            "ждать вас!").format(name=items[2])
 
-                    request = f"status=7, deleteauto_date='{datetime.now().date()}', " \
-                              f"recovery_date=null, rate_id=null, subscription_date=null"
+                        request = f"status=7, deleteauto_date='{datetime.now().date()}', " \
+                                  f"recovery_date=null, rate_id=null, subscription_date=null"
 
-                    await db.add_user_parameters(items[0], request)
+                        await db.add_user_parameters(items[0], request)
 
-                    await bot.send_message(items[0], text=text_del)
+                        await bot.send_message(items[0], text=text_del)
 
-                    await notify_operator_about_delete_user(items[0], items[-1])
+                        await notify_operator_about_delete_user(items[0], items[-1])
+                    except:
+                        pass
 
                 elif items[1] - timedelta(3) == datetime.now().date():
                     print(2)
-                    await db.update_user_parameters(user_id=items[0], parameter_name="status", parameter_value=3)
+                    try:
+                        await db.update_user_parameters(user_id=items[0], parameter_name="status", parameter_value=3)
 
-                    rate = await db.get_rates_info(items[0])
+                        rate = await db.get_rates_info(items[0])
 
-                    markup = InlineKeyboardMarkup(
-                        inline_keyboard=[
-                            [InlineKeyboardButton(text=_("Оплатить текущий тариф"), callback_data="pay")],
-                            [InlineKeyboardButton(text=_("Узнать про все тарифы"), callback_data="about")],
-                            [InlineKeyboardButton(text=_("Отказаться от подписки"), callback_data="cancel_sub")]
-                        ]
-                    )
+                        markup = InlineKeyboardMarkup(
+                            inline_keyboard=[
+                                [InlineKeyboardButton(text=_("Оплатить текущий тариф"), callback_data="pay")],
+                                [InlineKeyboardButton(text=_("Узнать про все тарифы"), callback_data="about")],
+                                [InlineKeyboardButton(text=_("Отказаться от подписки"), callback_data="cancel_sub")]
+                            ]
+                        )
 
-                    await bot.send_message(items[0],
-                                           text=_("Привет, {name}! Сообщаю, что пора продлить вашу подписку на "
-                     "телеграм-канал 'Пой со мной'. До окончания подписки осталось всего {day}. Сейчас у вас "
-                     "подписка с тарифом: {rate_name} ({price}р.). Кстати, сейчас вы можете сменить тариф "
-                     "подписки.").format(name=items[2], day="3 дня", rate_name=rate_name[rate[-1]],
-                                                            price=rate[0]),
-                                           reply_markup=markup)
+                        await bot.send_message(items[0],
+                                               text=_("Привет, {name}! Сообщаю, что пора продлить вашу подписку на "
+                                                      "телеграм-канал 'Пой со мной'. До окончания подписки осталось всего {day}. Сейчас у вас "
+                                                      "подписка с тарифом: {rate_name} ({price}р.). Кстати, сейчас вы можете сменить тариф "
+                                                      "подписки.").format(name=items[2], day="3 дня",
+                                                                          rate_name=rate_name[rate[-1]],
+                                                                          price=rate[0]),
+                                               reply_markup=markup)
+                    except:
+                        pass
                 elif items[1] - timedelta(2) == datetime.now().date():
                     print(3)
                     markup = InlineKeyboardMarkup(
@@ -72,16 +82,19 @@ async def check_access():
                             [InlineKeyboardButton(text=_("Отказаться от подписки"), callback_data="cancel_sub")]
                         ]
                     )
+                    try:
+                        rate = await db.get_rates_info(items[0])
 
-                    rate = await db.get_rates_info(items[0])
-
-                    await bot.send_message(items[0],
-                                           text=_("Привет, {name}! Сообщаю, что пора продлить вашу подписку на "
-                     "телеграм-канал 'Пой со мной'. До окончания подписки осталось всего {day}. Сейчас у вас "
-                     "подписка с тарифом: {rate_name} ({price}р.). Кстати, сейчас вы можете сменить тариф "
-                     "подписки.").format(name=items[2], day="2 дня", rate_name=rate_name[rate[-1]],
-                                                            price=rate[0]),
-                                           reply_markup=markup)
+                        await bot.send_message(items[0],
+                                               text=_("Привет, {name}! Сообщаю, что пора продлить вашу подписку на "
+                                                      "телеграм-канал 'Пой со мной'. До окончания подписки осталось всего {day}. Сейчас у вас "
+                                                      "подписка с тарифом: {rate_name} ({price}р.). Кстати, сейчас вы можете сменить тариф "
+                                                      "подписки.").format(name=items[2], day="2 дня",
+                                                                          rate_name=rate_name[rate[-1]],
+                                                                          price=rate[0]),
+                                               reply_markup=markup)
+                    except:
+                        pass
                 elif items[1] - timedelta(1) == datetime.now().date():
                     print(4)
                     markup = InlineKeyboardMarkup(
@@ -91,19 +104,25 @@ async def check_access():
                             [InlineKeyboardButton(text=_("Отказаться от подписки"), callback_data="cancel_sub")]
                         ]
                     )
+                    try:
+                        rate = await db.get_rates_info(items[0])
 
-                    rate = await db.get_rates_info(items[0])
-
-                    await bot.send_message(items[0],
-                                           text=_("Привет, {name}! Сообщаю, что пора продлить вашу подписку на "
-                     "телеграм-канал 'Пой со мной'. До окончания подписки осталось всего {day}. Сейчас у вас "
-                     "подписка с тарифом: {rate_name} ({price}р.). Кстати, сейчас вы можете сменить тариф "
-                     "подписки.").format(name=items[2], day="1 день", rate_name=rate_name[rate[-1]],
-                                                            price=rate[0]),
-                                           reply_markup=markup)
+                        await bot.send_message(items[0],
+                                               text=_("Привет, {name}! Сообщаю, что пора продлить вашу подписку на "
+                                                      "телеграм-канал 'Пой со мной'. До окончания подписки осталось всего {day}. Сейчас у вас "
+                                                      "подписка с тарифом: {rate_name} ({price}р.). Кстати, сейчас вы можете сменить тариф "
+                                                      "подписки.").format(name=items[2], day="1 день",
+                                                                          rate_name=rate_name[rate[-1]],
+                                                                          price=rate[0]),
+                                               reply_markup=markup)
+                    except:
+                        pass
                 else:
-                    await asyncio.sleep(60)
+                    print(3432432)
+                    await asyncio.sleep(1)
             else:
+                print(4343424)
                 await asyncio.sleep(60)
         else:
+            print(325435453534)
             await asyncio.sleep(60)
